225c7b022ca5435b78dadd813f96fc237204532c
ricardo-silvasantos <ricosilvasantos@yahoo.com.br>
2023-08-20 14:35:21 -0300

    resolução exercicio Pastorzinho

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Runtime.InteropServices.Marshalling;
+
+        Console.Beep(1320, 400);
+        Console.Beep(1485, 400);
+        Console.Beep(1650, 400);
+
+        Console.Beep(1759, 400);
+        Console.Beep(1759, 400);
+        Console.Beep(1759, 400);
+
+        Console.Beep(1320,400);
+        Console.Beep(1485,400);
+        Console.Beep(1320,400);
+
+        Console.Beep(1485,400);
+        Console.Beep(1485,400);
+        Console.Beep(1485,400);
+
+        Console.Beep(1320,400);
+        Console.Beep(1980,400);
+        Console.Beep(1759,400);
+
+        Console.Beep(1650,400);
+        Console.Beep(1650,400);
+        Console.Beep(1650,400);
+
+        Console.Beep(1320,400);
+        Console.Beep(1485,400);
+        Console.Beep(1650,400);
+
+        Console.Beep(1759,400);
+        Console.Beep(1759,400);
+        Console.Beep(1759,400);
